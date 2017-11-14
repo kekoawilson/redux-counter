48522ea67a7e6fe46f80b6ae64d228629984a79d
Kekoa Wilson <kekoawilson20@gmail.com>
2017-11-14 14:05:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,55 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { increment, decrement } from './ducks/counter';
+
 
 import "./App.css";
 
 export class App extends Component {
 	render() {
+		const { increment, decrement, undo, redo, currentValue } = this.props
+		console.log('props obj', this.props)
 		return (
 			<div className="app">
 				<section className="counter">
-					<h1 className="counter__current-value">{ 0 }</h1>
+					<h1 className="counter__current-value">{currentValue}</h1>
 					<div className="counter__button-wrapper">
 						<button
 							className="counter__button increment-one"
-							onClick={ () => null }
+							onClick={() => increment(1)}
 						>
 							+1
 						</button>
 						<button
 							className="counter__button increment-five"
-							onClick={ () => null }
+							onClick={() => increment(5)}
 						>
 							+5
 						</button>
 						<button
 							className="counter__button decrement-one"
-							onClick={ () => null }
+							onClick={() => decrement(1)}
 						>
 							-1
 						</button>
 						<button
 							className="counter__button decrement-five"
-							onClick={ () => null }
+							onClick={() => decrement(5)}
 						>
 							-5
 						</button>
 						<br />
 						<button
 							className="counter__button undo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={true}
+							onClick={() => null}
 						>
 							Undo
 						</button>
 						<button
 							className="counter__button redo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={true}
+							onClick={() => null}
 						>
 							Redo
 						</button>
@@ -52,7 +57,7 @@ export class App extends Component {
 				</section>
 				<section className="state">
 					<pre>
-						{ JSON.stringify( this.props, null, 2 ) }
+						{JSON.stringify(this.props, null, 2)}
 					</pre>
 				</section>
 			</div>
@@ -60,4 +65,12 @@ export class App extends Component {
 	}
 }
 
-export default App;
+function mapStateToProps(state) {
+	console.log('State obj', state)
+	return state
+}
+
+const decorator = connect(mapStateToProps, { increment, decrement});
+const decoratedComp = decorator(App)
+
+export default decoratedComp;
